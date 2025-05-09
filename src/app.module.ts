46f4998ebde5508745a59b25303681eab0cdbeb9
redaezziani <klausdev2@gmail.com>
2025-05-09 21:32:49 +0100

    feat: add Swagger documentation and event emitter support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [EventEmitterModule.forRoot()],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      skipMissingProperties: false,
+      validationError: {
+        target: false,
+        value: false,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Zwin Camp')
+    .setDescription(
+      'Api For Zwin Camp, The best place for finding the best camps in your city and fast planning',
+    )
+    .setVersion('1.0')
+    .addTag('camps')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
